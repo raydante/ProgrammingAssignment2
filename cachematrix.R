4b5755cf12b1bcf7561e868603adce33ea3b05cb
Ray Descalzo <raydescalzo@gmail.com>
2015-01-14 16:53:01 -0800

    Added code to skeleton functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix wraps a matrix in getter and setter functions while
+## cacheSolve returns the inverse using cached results or solve()
 
-## Write a short comment describing this function
+## Takes a matrix x and wraps it in getter and setter functions for the 
+## matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y) {
+            x <<- y
+            i <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) i <<- inverse
+        getInverse <- function() i
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+    }
 
-}
 
 
-## Write a short comment describing this function
+
+## Takes the output of makeCacheMatrix and returns stored inverse if there, 
+## otherwise finds inverse via solve() then caches and returns it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        # message("using cached inverse")
+        return(inv)
+    }
+    m8trix <- x$get()
+    inv <- solve(m8trix,...)
+    x$setInverse(inv)
+    inv
 }
+
+
